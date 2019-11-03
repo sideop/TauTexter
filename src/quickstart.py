d1d8ad8cddd6e421d8278c2d49d16a216d901afe
Neo Huang <41600737+sideop@users.noreply.github.com>
2019-11-03 16:22:03 -0600

    Update quickstart.py

--- src/quickstart.py
+++ src/quickstart.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACc16695dcdd43a77dbb7f42478a27ff3f'
-auth_token = '1708536a7fe2874d50351d9d8fbc994f'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 message = client.messages \
